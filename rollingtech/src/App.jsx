064f6e04caa65ed908624177f02b638aa2c4e2fc
Diego Fenino <diegofenino@gmail.com>
2023-07-07 17:32:41 -0300

    errorPage

--- rollingtech/src/App.jsx
+++ rollingtech/src/App.jsx
@@ -3,18 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { AdminPage } from './pages/AdminPage'
 import { Navegador } from './componentes/Nav'
+import { Error404 } from './pages/ErrorPage'
+import { Footer } from './componentes/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Navegador />}>
-        <Route index element={<HomePage />} />
-        <Route path='admin' element={<AdminPage />}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Navegador />}>
+          <Route index element={<HomePage />} />
+          <Route path='admin' element={<AdminPage />} />
+        </Route>
+      </Routes>
 
     </>
   )
--- rollingtech/src/componentes/Nav.jsx
+++ rollingtech/src/componentes/Nav.jsx
@@ -1,32 +1,51 @@
 import React from 'react'
-import {Link, Outlet} from "react-router-dom"
-import {Container, Navbar} from 'react-bootstrap'
+import { Link, Outlet } from "react-router-dom"
+import { Container, Navbar, Nav, Button, Form } from 'react-bootstrap'
+import { Footer } from './Footer'
 
 export const Navegador = () => {
   return <>
-  
-  <Navbar expand="md"  bg="dark" data-bs-theme="dark">
-    <Container fluid>
-      <Navbar.Brand>
-      <img
-              alt=""
-              src="https://w7.pngwing.com/pngs/568/379/png-transparent-technology-computer-icons-technology-electronics-text-logo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}      
-          <Link to='/' className='text-light'>Rolling Tech</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-      <div>
-          <Link to='admin' className='btn btn-light'>Admin Page</Link>
-          <Link to='/' className='btn btn-light'>Home Page</Link>
-      </div>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  <Outlet />
+    
+    <Navbar expand="lg" bg="primary" data-bs-theme="dark">
+      <Container fluid>
+        <img
+            alt=""
+            src="https://w7.pngwing.com/pngs/568/379/png-transparent-technology-computer-icons-technology-electronics-text-logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top mx-3"
+          />{' '}
+        <Navbar.Brand href="#">Rolling Tech</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll"/>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-2">|</Nav>
+          <Nav
+            className="me-auto"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Destacado</Nav.Link>
+            <Nav.Link href="#action3">Contacto</Nav.Link>
+            <Nav.Link href="#action4">Favoritos</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button className="me-5" variant="outline-ligth">Search</Button>
+          </Form>
+          <Nav>
+            <Nav.Link className="me-2" href="#action5">Ingresar</Nav.Link>
+            <Nav.Link className="me-3" href="#action6">Mi carrito</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Footer/>
   </>
-  
+
 }
