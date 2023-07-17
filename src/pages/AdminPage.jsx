6816e49b52b922e6fa3b57ff5f00cad827657a3e
Ignacio Guardo <nachoguardoruiz@gmail.com>
2023-07-17 18:59:49 -0300

    se agregaron rutas de pagina admin

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,17 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
 export const AdminPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="m-5 container-fluid row gap-4 justify-content-center">
-        <button className="p-4 bg-dark text-light col-3 text-center btn">
+        <button
+          className="p-4 bg-dark text-light col-3 text-center btn"
+          onClick={() => navigate("/admin/usuarios")}
+        >
           Usuarios
         </button>
-        <button className="p-4 bg-dark text-light col-3 text-center btn">
+        <button
+          className="p-4 bg-dark text-light col-3 text-center btn"
+          onClick={() => navigate("/admin/ventas")}
+        >
           Ventas
         </button>
-        <button className="p-4 bg-dark text-light col-3 text-center btn">
+        <button
+          className="p-4 bg-dark text-light col-3 text-center btn"
+          onClick={() => navigate("/admin/inventario")}
+        >
           Inventario
         </button>
-        <button className="p-4 bg-dark text-light col-3 text-center btn">
+        <button
+          className="p-4 bg-dark text-light col-3 text-center btn"
+          onClick={() => navigate("/admin/administradores")}
+        >
           Administradores
         </button>
       </div>
