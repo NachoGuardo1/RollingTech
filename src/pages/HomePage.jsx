07064534874493a512825052210e738811c980ef
Ignacio Guardo <nachoguardoruiz@gmail.com>
2023-07-27 22:11:49 -0300

    muestra de productos de BD en home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Paginacion from "../componentes/Paginacion";
 import ProductList from "../componentes/ProductList";
 import { DropdownCategoria } from "../componentes/DropdownCategoria";
-import productos from "../data/data";
 
 export const HomePage = () => {
   const [paginaActual, setPaginaActual] = useState(1);
@@ -18,22 +17,20 @@ export const HomePage = () => {
     setPaginaActual((paginaAnterior) => paginaAnterior - 1);
   };
 
-  const products = productos;
-
   return (
     <div className="p-0 m-0 d-flex row container-fluid justify-content-center">
       <Carrousel />
       <div>
         <DropdownCategoria
           categorias={[
-            "celulares",
-            "notebooks",
-            "tablets",
-            "televisores",
-            "consolas",
+            "CELULARES",
+            "NOTEBOOKS",
+            "TABLETS",
+            "TELEVISORES",
+            "CONSOLAS",
           ]}
         />
-        <ProductList products={products} />
+        <ProductList />
       </div>
       <Paginacion
         paginaActual={paginaActual}
