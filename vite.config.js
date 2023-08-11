6ab9c525b0ac783a28e9e36973126d4664e2fe19
aeiturre <aeiturre@gmail.com>
2023-08-11 16:35:03 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    // genera el archivo manifest.json en outDir
+    manifest: true,
+    rollupOptions: {
+      // sobreescribe la entrada por defecto .html
+      input: '/src/main.jsx'
+    }
+  }
+
 })
+
